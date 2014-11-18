72de0fd6a5f22751b5b9ee60637a211f5f8427e2
NataliGil <giliaskin@ymail.com>
2014-11-18 12:53:18 +0200

    php files for FriendlyLiveTranslator project

---
+++ include/config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Database config variables
+ */
+define("DB_HOST", "mysql6.000webhost.com");
+define("DB_USER", "a7318172_gil");
+define("DB_PASSWORD", "flt2014");
+define("DB_DATABASE", "a7318172_login");
+?>
